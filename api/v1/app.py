a68536d7da7c23622cf91761819a288991df1167
Mariana <marianaplazas@live.com>
2019-09-10 21:11:02 +0000

    add all api

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+module of api
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ api/v1/views/index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns ok"""
+    return (jsonify({"status": "OK"}))
