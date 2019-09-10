ea919873d123f34d25f643d53ba7b76719d53fd0
Mariana <marianaplazas@live.com>
2019-09-10 23:37:09 +0000

    add index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+import models
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns ok"""
     return (jsonify({"status": "OK"}))
+
+@app_number_of_objects.route('/api/v1/stats', strict_slashes=False)
+def endpoint():
+    """define an endpoint that retrives the number 
+       of objects by type"""
+    return (jsonify({"amenities": storage.count("Amenity"), 
+                     "cities": storage.count("City"),
+                     "places": storage.count("Place"),
+                     "reviews": storage.count("Review"),
+                     "states": storage.count("State"),
+                     "users": storage.count("User")}))
