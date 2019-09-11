3e0c52d40574856417f6a33accd2d7a1e7e101d5
Mariana <marianaplazas@live.com>
2019-09-11 16:01:33 +0000

    fix pep8 in the index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,16 +4,18 @@ from flask import jsonify
 from models import storage
 import models
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """returns ok"""
     return (jsonify({"status": "OK"}))
 
+
 @app_number_of_objects.route('/api/v1/stats', strict_slashes=False)
 def endpoint():
-    """define an endpoint that retrives the number 
+    """define an endpoint that retrives the number
        of objects by type"""
-    return (jsonify({"amenities": storage.count("Amenity"), 
+    return (jsonify({"amenities": storage.count("Amenity"),
                      "cities": storage.count("City"),
                      "places": storage.count("Place"),
                      "reviews": storage.count("Review"),
